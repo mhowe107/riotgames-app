2c3e55e67ee1370140c7c8da72aa42532d50d5bb
mhowe107 <mhowe107@gmail.com>
2022-01-26 21:14:42 -0500

    Pushed initial changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './navigation/topnavbar.component';
+import { BottomNavBarComponent } from './navigation/bottomnavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    BottomNavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/navigation/bottomnavbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'riot-bottomnavbar',
+    templateUrl: 'bottomnavbar.component.html',
+    styleUrls: ['bottomnavbar.component.css']
+})
+
+export class BottomNavBarComponent {
+
+}
