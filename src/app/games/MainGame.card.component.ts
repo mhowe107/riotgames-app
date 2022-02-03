07fd800c40509e96c2bf6a4aaa2980f357f5f1f5
mhowe107 <mhowe107@gmail.com>
2022-02-02 20:35:23 -0500

    Added Large and Small Game Cards

---
+++ src/app/games/MainGame.card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'riot-games-card',
+    templateUrl: 'MainGame.card.component.html',
+    styleUrls: ['MainGame.card.component.css']
+})
+
+export class MainGameCardComponent {
+
+}
