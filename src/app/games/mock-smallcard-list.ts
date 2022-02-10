42222e117f633a54ad6107da043b1be6bffea5f0
mhowe107 <mhowe107@gmail.com>
2022-02-09 23:17:53 -0500

    Added more smallcards, completed data binding

--- src/app/games/mock-smallcard-list.ts
+++ src/app/games/mock-smallcard-list.ts
@@ -3,4 +3,12 @@ export const mock_smallcard_list = [
         image:"./assets/tft-card-bg-1800.jpg",
         logo:"./assets/tft-logotype-2021.png",
     },
+    {
+        image:"./assets/legends-of-runeterra-productimage.png",
+        logo:"./assets/legends-of-runeterra-logotype.png",
+    },
+    {
+        image:"./assets/lol-wildrift-productimage.png",
+        logo:"./assets/lol-wildrift-logotype.png",
+    },
 ];
