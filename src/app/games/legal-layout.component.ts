261c8704e55ada7b752d672f7362259ac5634bc3
mhowe107 <mhowe107@gmail.com>
2022-02-16 22:05:06 -0500

    Added Routing for all navbar links

---
+++ src/app/games/legal-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'riot-legal-layout',
+    templateUrl: 'legal-layout.component.html'
+})
+
+export class LegalLayoutComponent {
+
+}
